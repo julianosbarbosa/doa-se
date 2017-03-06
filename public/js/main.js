4ffa8df7cc937f3fc85efe0c3be703faba337a75
Juliano <juliano@nationphone.com>
2017-03-06 00:09:39 -0300

    UPDATE - Comentarios no codigo e orientacoes de funcionamento do angular

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 angular.module('doa-se', ['minhasDiretivas', 'ngAnimate', 'ngRoute', 'ngResource', 'meusServicos'])
     //modulo principal usa o objeto global>>angular. tem 2 parametros primeiro o nome do app e o segundo é um arrey com os modulos que vao ser usados
-    .config(function($routeProvider, $locationProvider) {
-
-        $routeProvider.when('/fotos', {
-            templateUrl: 'partials/principal.html',
-            controller: 'FotosController'
+    .config(function($routeProvider, $locationProvider) { //a funçao $routeProvider do ngRoute cnfigura rotas
+$locationProvider.html5Mode(true);
+//$locationProvider faz com que a url nao apareça o #
+        $routeProvider.when('/fotos', { //quando tiver no browser /fotos
+            templateUrl: 'partials/principal.html',  //chama o template partials/principal.html
+            controller: 'FotosController' //e o controller que esta sendo usado 
         });
 
         $routeProvider.when('/fotos/new', {
@@ -17,6 +18,6 @@ angular.module('doa-se', ['minhasDiretivas', 'ngAnimate', 'ngRoute', 'ngResource
             controller: 'FotoController'
         });
 
-        $routeProvider.otherwise({ redirectTo: '/fotos' });
+        $routeProvider.otherwise({ redirectTo: '/fotos' }); //otherwise caso suario use uma rota que nao exite ele faz um redirectTo p /fotos
 
     });
