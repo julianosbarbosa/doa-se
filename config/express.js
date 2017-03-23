89623235b76bbcd4cc32d80ec90cad91d8d2b339
Juliano <juliano@nationphone.com>
2017-03-22 23:46:12 -0300

    UPDATE - Detalhes de configuração do servidor nodejs com express

--- config/express.js
+++ config/express.js
@@ -1,12 +1,12 @@
-var express = require('express')
-    ,app = express()
-    ,bodyParser = require('body-parser')
-    ,routes = require('../app/routes');
+var express = require('express'), //modulo que executa as requisições de cada pagina que é solicitado ao servidor
+    app = express(), //estancio meu express no app 
+    bodyParser = require('body-parser'),
+    routes = require('../app/routes');
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public')); // static medleware que passa a pasta que vai ser assistida pelo express
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app);
 
-module.exports = app;
+module.exports = app; // exporta para o meu server.js
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var http = require('http')
-    ,app = require('./config/express')
+var http = require('http'), // http o modulo que cria o servidor
+    app = require('./config/express'), //estancio meu express no app 
     db = require('./config/database');
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(3000, function() { // http.createServer(app executa as requisições e resposta de cada pagina) cria o servidor .listen ouve a porta 3000 e passo uma function de segundo parametro
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
 
+//apos criar o Servidor fazer o npm init
